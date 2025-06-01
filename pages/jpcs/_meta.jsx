83d529b7882b120a0e7eb0234e76bfeb15c7eec6
0x6980 <0x6980@gmail.com>
2025-06-01 11:31:41 +0330

    Jagged assist added

--- pages/jpcs/_meta.jsx
+++ pages/jpcs/_meta.jsx
@@ -1,7 +1,8 @@
 export default {
   index: 'Intro',
-  jagged_function: 'Jagged function',
+  jagged_function: 'Jagged Function',
   jagged_basic: 'Jagged Basic',
+  jagged_assist: 'Jagged Assist',
   fancy_jagged: 'Fancy Jagged',
   read_once_branching_program: 'Read-Once Branching Programs',
   matrix_branching_program: 'Matrix Branching Program',
